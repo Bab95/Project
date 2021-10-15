d0f3a97dcd3e782133c4145c8545507760ef2ec5
Babnish Vyas <babnishvyas@microsoft.com>
2021-10-16 01:01:50 +0530

    adding loading component

--- FrontEnd/client-app/src/app/api/agent.ts
+++ FrontEnd/client-app/src/app/api/agent.ts
@@ -4,6 +4,21 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 
 const responseBody = <T> (response : AxiosResponse<T>) => response.data;
 
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+    })
+}
+
+axios.interceptors.response.use(async response => {
+    try{
+        await sleep(1000);
+        return response;
+    }catch(error){
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
 const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
     post: <T> (url:string, body:{}) => axios.post<T>(url).then(responseBody),
